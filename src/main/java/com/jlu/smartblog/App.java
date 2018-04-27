5b33b779604f67b08f6132b2810505dda917973e
Easoncheng0405 <597021782@qq.com>
2018-04-27 17:01:02 +0800

    设计首页样式

---
+++ src/main/java/com/jlu/smartblog/App.java
@@ -0,0 +1,19 @@
+package com.jlu.smartblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IDEA
+ * author:程杰
+ * Date:2018/4/26
+ * github:Easoncheng0405
+ */
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args){
+        SpringApplication.run(App.class,args);
+    }
+}
