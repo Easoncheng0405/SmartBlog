ed0fd1ab775e631ac6270d0a569d3263017a9162
Easoncheng0405 <597021782@qq.com>
2018-04-27 23:22:25 +0800

    加入博客编辑页面

---
+++ src/main/java/com/jlu/smartblog/controller/EditorController.java
@@ -0,0 +1,21 @@
+package com.jlu.smartblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created with IDEA
+ * author:程杰
+ * Date:2018/4/27
+ * github:Easoncheng0405
+ */
+@Controller
+@RequestMapping("/edit")
+public class EditorController {
+
+    @GetMapping
+    public String get(){
+        return "edit";
+    }
+}
