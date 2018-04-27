c9ccc6a7d9c6472616a5460c57d189570f920831
Easoncheng0405 <597021782@qq.com>
2018-04-27 18:06:57 +0800

    优化首页样式

--- src/main/java/com/jlu/smartblog/controller/IndexController.java
+++ src/main/java/com/jlu/smartblog/controller/IndexController.java
@@ -2,9 +2,7 @@ package com.jlu.smartblog.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
 import java.util.Map;
